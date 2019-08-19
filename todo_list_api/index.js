e09becb72091af9de91f131a8e704a694f436c60
Henry Wang <henry.wang@live.com>
2019-08-18 18:03:40 -0700

    first commit

---
+++ todo_list_api/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
+
+
+var todoRoutes = require('./routes/todos');
+app.use('/api/todos', todoRoutes);
+
+
+app.get('/', function(req, res){
+   res.sendFile('index.html');
+});
+
+app.listen(port, function(){
+   console.log("Server Running: " + port); 
+});
